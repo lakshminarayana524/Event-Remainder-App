ccf1cb8a1d32bfc2683d9c5ce0fae21c1c49033d
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-25 22:47:18 +0530

    changed session verification

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials=true;
+
 const api = axios.create({
     baseURL:"https://event-remainder-app.onrender.com/api",
     // baseURL: 'http://localhost:5000/api',
