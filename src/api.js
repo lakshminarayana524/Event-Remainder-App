6585eba33a27be58af5af90b81f0e1d82707182f
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-27 23:13:59 +0530

    changed port

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://event-remainder-app.onrender.com/api',
-    baseURL:'http://localhost:5000/api',
+    baseURL: 'https://event-remainder-app.onrender.com/api',
+    // baseURL:'http://localhost:5000/api',
     withCredentials: true,
 });
 
