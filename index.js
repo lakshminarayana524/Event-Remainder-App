e2ed57dc6d1549f532774fcd33ead3699f4d8736
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-27 23:17:04 +0530

    changed port

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 3, // 3 hours
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        secure: true, // Use secure cookies in production
         sameSite: 'strict' // Important for cross-origin requests
     }
 }));
