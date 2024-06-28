3c7a12340f6cc98697b610bfc5b667380a6a2f4b
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-28 09:47:31 +0530

    checking token verification

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db');
 const authRouter = require('./routes/authRoutes');
 const eventRouter = require('./routes/eventRoutes');
-const sendEmail = require('./config/mail')
+const sendEmail = require('./config/mail');
 
 const app = express();
 
@@ -19,10 +19,9 @@ connectDB();
 
 // Middleware setup
 app.use(cors({
-    origin: "https://my-event-remainder.vercel.app",
-    // origin:"http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+  origin: process.env.NODE_ENV === 'production' ? "https://my-event-remainder.vercel.app" : "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 
 app.use(bodyParser.json());
@@ -31,37 +30,38 @@ app.use(cookieParser());
 
 // Session setup
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 3, // 3 hours
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // Important for cross-origin requests
-    }
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 3, // 3 hours
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // Important for cross-origin requests
+  }
 }));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send("Hi, I am Here");
+  res.send("Hi, I am Here");
 });
 
 app.get('/send-test-email', async (req, res) => {
-    try {
-        await sendEmail('myeventremainder@gmail.com', 'Test Subject', 'Test email content');
-        res.send('Test email sent successfully');
-    } catch (error) {
-        console.error('Error sending test email:', error);
-        res.status(500).send('Error sending test email');
-    }
+  try {
+    await sendEmail('myeventremainder@gmail.com', 'Test Subject', 'Test email content');
+    res.send('Test email sent successfully');
+  } catch (error) {
+    console.error('Error sending test email:', error);
+    res.status(500).send('Error sending test email');
+  }
 });
+
 app.use('/api/auth', authRouter);
 app.use('/api/event', eventRouter);
 
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server is running on " + port);
+  console.log("Server is running on " + port);
 });
