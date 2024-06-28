10f20b627f51ee90d8caaab66cb019b1b6c4a235
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-28 09:48:51 +0530

    resolving methods

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 // api.js
 import axios from 'axios';
 
+axios.defaults.withCredentials=true;
+
 const api = axios.create({
     baseURL: 'https://event-remainder-app.onrender.com/api',
     // baseURL:'http://localhost:5000/api',
