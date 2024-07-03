dc553941e6e384cd45433ade57fcc8af2825c797
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-07-03 14:40:32 +0530

    added credintials details

--- src/api.js
+++ src/api.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+axios.defaults.withCredentials = true
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL:'https://event-remainder-app.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use(config => {
